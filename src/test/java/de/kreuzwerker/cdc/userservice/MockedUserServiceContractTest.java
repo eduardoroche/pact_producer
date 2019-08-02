8654927c4b654d74e9c23ca90b18b057beaf2b53
Eduardo Raupp Pereira <eduardo.raupp_pereira@roche.com>
2019-08-02 10:44:26 +0200

    changed name

--- src/test/java/de/kreuzwerker/cdc/userservice/MockedUserServiceContractTest.java
+++ src/test/java/de/kreuzwerker/cdc/userservice/MockedUserServiceContractTest.java
@@ -1,11 +1,11 @@
 package de.kreuzwerker.cdc.userservice;
 
+import au.com.dius.pact.provider.junit.PactRunner;
 import au.com.dius.pact.provider.junit.Provider;
 import au.com.dius.pact.provider.junit.State;
 import au.com.dius.pact.provider.junit.loader.PactBroker;
 import au.com.dius.pact.provider.junit.target.Target;
 import au.com.dius.pact.provider.junit.target.TestTarget;
-import au.com.dius.pact.provider.spring.SpringRestPactRunner;
 import au.com.dius.pact.provider.spring.target.SpringBootHttpTarget;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,10 +19,10 @@ import java.util.UUID;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-@RunWith(SpringRestPactRunner.class)
+@RunWith(PactRunner.class)
 @Provider("user-service")
 //@PactFolder("pacts")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 //@Ignore
 @PactBroker(host = "pact_broker", tags = "${pactbroker.tags:master}")
 public class MockedUserServiceContractTest {
