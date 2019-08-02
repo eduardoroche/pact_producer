ee36aa5e25b566240b4fa9cf5c598001be167ade
Eduardo Raupp Pereira <eduardo.raupp_pereira@roche.com>
2019-08-02 09:53:27 +0200

    blabla

--- src/main/java/io/reflectoring/MessageProviderConfiguration.java
+++ src/main/java/io/reflectoring/MessageProviderConfiguration.java
@@ -2,6 +2,8 @@ package io.reflectoring;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +18,20 @@ class MessageProviderConfiguration {
 		return new TopicExchange("myExchange");
 	}
 
+
+	@Bean
+	public ConnectionFactory connectionFactory() {
+		CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
+		connectionFactory.setUsername("guest");
+		connectionFactory.setPassword("guest");
+		return connectionFactory;
+	}
+
+	@Bean
+	public RabbitTemplate rabbitTemplate() {
+		RabbitTemplate template = new RabbitTemplate(connectionFactory());
+		return template;
+	}
 
 	@Bean
 	MessageProducer messageProvider(MessagePublisher publisher) {
